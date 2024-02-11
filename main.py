5bd408c9b6428247d7e6e0f1e7d33e6cf376d457
Karyna <karyna_7@ukr.net>
2024-02-11 16:06:13 +0200

    Added minimum

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # task_1
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+#
+# if n1 <= n2 <= n3:
+#     print(f"Min: {n1}")
+# elif n1 <= n3 <= n2:
+#     print(f"Min: {n1}")
+# elif n2 <= n1 <= n3:
+#     print(f"Min: {n2}")
+# elif n2 <= n3 <= n1:
+#     print(f"Min: {n2}")
+# elif n3 <= n1 <= n2:
+#     print(f"Min: {n3}")
+# elif n3 <= n2 <= n1:
+#     print(f"Min: {n3}")
