0d968b0eb9ef1c4f5049cca76e760d32d033b7da
Karyna <karyna_7@ukr.net>
2024-02-11 16:06:41 +0200

    Added maximum

--- main.py
+++ main.py
@@ -15,3 +15,16 @@
 #     print(f"Min: {n3}")
 # elif n3 <= n2 <= n1:
 #     print(f"Min: {n3}")
+#
+# if n1 >= n2 >= n3:
+#     print(f"Max: {n1}")
+# elif n1 >= n3 >= n2:
+#     print(f"Max: {n1}")
+# elif n2 >= n1 >= n3:
+#     print(f"Max: {n2}")
+# elif n2 >= n3 >= n1:
+#     print(f"Max: {n2}")
+# elif n3 >= n1 >= n2:
+#     print(f"Max: {n3}")
+# elif n3 >= n2 >= n1:
+#     print(f"Max: {n3}")
